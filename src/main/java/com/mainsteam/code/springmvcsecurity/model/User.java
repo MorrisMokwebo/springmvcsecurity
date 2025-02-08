301f3593768de5c451e361863ccb5a435775f375
morris <mokwebomorris@gmail.com>
2025-02-08 12:44:11 +0200

    creating user object and user dao

---
+++ src/main/java/com/mainsteam/code/springmvcsecurity/model/User.java
@@ -0,0 +1,125 @@
+package com.mainsteam.code.springmvcsecurity.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1,message = "first name should have more than one characters")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1,message = "last name should have more than one characters")
+    private String lastName;
+
+    @NotNull
+    @Email
+    @Size(min = 1,message = "email should have more than one characters")
+    private String email;
+
+    @NotNull
+    @Size(max = 10, message = "phone numbers should have 10 digits")
+    private String phoneNumber;
+
+    @NotNull
+    @Size(min = 1, message = "username should have more that one characters")
+    private String username;
+
+    @NotNull
+    @Size(min = 1, message = "password should have more than one value")
+    private String password;
+
+    @NotNull
+    @Size(min = 1, message = "password should have more than one value")
+    private String confirmPassword;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String phoneNumber,
+                String username, String password, String confirmPassword) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
