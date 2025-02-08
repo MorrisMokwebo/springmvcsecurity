157d05e6b06d51e30fa36f99d17e08002dd2b7d4
morris <mokwebomorris@gmail.com>
2025-02-08 12:27:39 +0200

    update annotation

--- src/main/java/com/mainsteam/code/springmvcsecurity/SecurityConfig.java
+++ src/main/java/com/mainsteam/code/springmvcsecurity/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.mainsteam.code.springmvcsecurity;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 public class SecurityConfig {
 
     @Bean
