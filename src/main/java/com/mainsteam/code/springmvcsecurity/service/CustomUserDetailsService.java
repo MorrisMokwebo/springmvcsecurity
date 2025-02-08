0386783091fc60e448205b69490ca03c26d5aec3
morris <mokwebomorris@gmail.com>
2025-02-08 13:22:40 +0200

    creating custom userDetailsService

---
+++ src/main/java/com/mainsteam/code/springmvcsecurity/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.mainsteam.code.springmvcsecurity.service;
+
+import com.mainsteam.code.springmvcsecurity.model.User;
+import com.mainsteam.code.springmvcsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User  user = userRepository.findByUsername(username);
+
+        if(user == null){
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles("USER")
+                .build();
+    }
+}
